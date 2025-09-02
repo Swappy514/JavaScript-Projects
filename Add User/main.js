0c8df32a23bf5205b9185fcab0673328077b4fc3
Swappy514 <swaroopjadhav5@gmail.com>
2025-09-02 12:00:00 +0530

    2 Sep: Added main.js in Add User

---
+++ Add User/main.js
@@ -0,0 +1,26 @@
+let addUserFormE1 = document.getElementById("addUserForm");
+let nameE1 = document.getElementById("name");
+let nameErrMsgE1 = document.getElementById("nameErrMsg");
+
+let emailE1 = document.getElementById("email");
+let emailErrMsgE1 = document.getElementById("emailErrMsg");
+
+nameE1.addEventListener("blur", function(event) {
+    if (event.target.value === "") {
+        nameErrMsgE1.textContent = "Required*";
+    } else {
+        nameErrMsgE1.textContent = "";
+    }
+});
+
+emailE1.addEventListener("blur", function(event) {
+    if (event.target.value === "") {
+        emailErrMsgE1.textContent = "Required*";
+    } else {
+        emailErrMsgE1.textContent = "";
+    }
+});
+
+addUserFormE1.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
