65082e3d3887a02de627977082184d76067a8a70
Swappy514 <swaroopjadhav5@gmail.com>
2025-09-06 12:36:01 +0530

    add js file

---
+++ Select your Pet/main.js
@@ -0,0 +1,16 @@
+let petsImageUrls = {
+    dog: "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-dog-img.png",
+    cat: "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-cat-img.png",
+    parrot: "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-parrot-img.png",
+    spider: "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-spider-img.png",
+    rabbit: "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-rabbit-img.png"
+};
+
+let petSelect = document.getElementById("petSelect");
+let petImg = document.getElementById("petImg");
+
+petSelect.addEventListener("change", function(event) {
+    let selectedPet = event.target.value;
+    let selectedPetImgUrl = petsImageUrls[selectedPet];
+    petImg.src = selectedPetImgUrl;
+});
